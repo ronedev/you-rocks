b8bfae321ff33d5788825fd836c0462a3cb22b7a
Agustin notebook <ronedev25@gmail.com>
2022-08-22 11:09:39 -0300

    add: products images and productController

---
+++ server/controllers/ProductController.js
@@ -0,0 +1,6 @@
+
+exports.addNewProduct = async (req, res, next)=>{
+    console.log(req.body)
+    return next()
+}
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const productController = require('../controllers/ProductController.js')
 
 const router = express.Router()
 
 //Home page
 router.get('/', (req, res)=> res.send('Hola'))
+
+//Agregar producto
+router.post('/product/add', productController.addNewProduct)
 
 module.exports = router
