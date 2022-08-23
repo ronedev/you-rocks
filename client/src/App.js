19c5ea2ebe40e700ab0e119102244005e63d8a0d
Agustin notebook <ronedev25@gmail.com>
2022-08-23 10:50:53 -0300

    add: Home page and frontend routes with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,18 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./pages/Home.js";
+
 function App() {
   return (
-    <div className="App">
-      <form action="http://localhost:3002/product/add" method="POST">
-        <input type="text" placeholder='title' name='title' />
-        <input type="number" placeholder='price' name='price' />
-        <select name="category">
-          <option value="shoes">Shoes</option>
-          <option value="t-shirts">T-shirts</option>
-          <option value="chaquet">Chaquet</option>
-          <option value="calzon">Calzon</option>
-        </select>
-        <input type="text" placeholder='enterprise' name='enterprise' />
-        <input type="radio" name="offer" />
-        <select name="gender">
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="unisex">Unisex</option>
-        </select>
-        <input type="number" name="quantity" placeholder="quantity" />
-        <input type="text" name="description" placeholder="description" />
-        <input type="hidden" name="sizes" value={['s', 'm', 'l', 'xxl']} />
-        <input type="submit" value="Enviar" />
-      </form>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    console.log('home')
+  return (
+    <>
+      <div style={{color: 'white'}}>Home</div>
+    </>
+  );
+};
+
+export default Home;
