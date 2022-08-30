a2f680c4d97ccee9a34671b483e9d328a42fd28c
Agustin notebook <ronedev25@gmail.com>
2022-08-29 23:52:15 -0300

    add: Product card of the store page

--- client/src/components/Store/ProductCard.js
+++ client/src/components/Store/ProductCard.js
@@ -1,27 +1,57 @@
-import React from 'react'
+import React from "react";
 
-const ProductCard = ({product}) => {
+const ProductCard = ({ product }) => {
   return (
-    <article className='itemContainer product'>
-        <div className="image" style={{backgroundImage: `url(../${product.images[0]})` }}>
+    <article className="itemContainer product">
+      <div
+        className="image"
+        style={{ backgroundImage: `url(../${product.images[0]})` }}
+      ></div>
+      <div className="title-price">
+        <h3>{product.title}</h3>
+        <span>${product.price}</span>
+      </div>
+      <div className="description">
+        <span>
+          {product.description.length > 60
+            ? product.description.substr(0, 60) + "..."
+            : product.description}
+        </span>
+      </div>
+      <div className="sizes">
+        <div
+          className={product.sizes.includes("s") ? "talle habilitado" : "talle"}
+        >
+          s
         </div>
-        <div className="title-price">
-            <h3>{product.title}</h3>
-            <span>${product.price}</span>
+        <div
+          className={product.sizes.includes("m") ? "talle habilitado" : "talle"}
+        >
+          m
         </div>
-        <div className="description">
-            <span>{product.description.length > 60 ? product.description.substr(0, 60) + '...' : product.description}</span>
+        <div
+          className={product.sizes.includes("l") ? "talle habilitado" : "talle"}
+        >
+          l
         </div>
-        <div className="sizes">
-          <div className={product.sizes.includes('s') ? 'talle habilitado' : 'talle'} >s</div>
-          <div className={product.sizes.includes('m') ? 'talle habilitado' : 'talle'} >m</div>
-          <div className={product.sizes.includes('l') ? 'talle habilitado' : 'talle'}>l</div>
-          <div className={product.sizes.includes('xl') ? 'talle habilitado' : 'talle'}>xl</div>
-          <div className={product.sizes.includes('xxl') ? 'talle habilitado' : 'talle'}>xxl</div>
+        <div
+          className={
+            product.sizes.includes("xl") ? "talle habilitado" : "talle"
+          }
+        >
+          xl
         </div>
-            <button className='btn btn-background'>Agregar al carrito</button>
+        <div
+          className={
+            product.sizes.includes("xxl") ? "talle habilitado" : "talle"
+          }
+        >
+          xxl
+        </div>
+      </div>
+      <button className="btn btn-background">Agregar al carrito</button>
     </article>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
