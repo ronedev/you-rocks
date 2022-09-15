c75fa9c5f90a8a05a02e92cef0c23075d3f4389d
Agustin notebook <ronedev25@gmail.com>
2022-09-15 10:29:06 -0300

    add: responsive navbar styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,23 @@
-import React, { useContext } from "react";
-import cartIcon from '../images/icons/cart.png'
+import React, { useContext, useState } from "react";
+import cartIcon from "../images/icons/cart.png";
+import menuIcon from "../images/icons/menu.png";
 import { UserContext } from "../context/UserContext";
 import { logoutUser } from "../services/user";
 import { CartContext } from "../context/CartContext";
 
 const Navbar = () => {
+  const { actualUser, setActualUser } = useContext(UserContext);
+  const { cartQuantity, openCart } = useContext(CartContext);
 
-  const { actualUser, setActualUser } = useContext(UserContext)
-  const { cartQuantity, openCart } = useContext(CartContext)
+  const [openMenu, setOpenMenu] = useState(false)
 
-  async function logout(){
-    const res = await logoutUser()
-    if(res.success){
-      setActualUser(null)
+  async function logout() {
+    const res = await logoutUser();
+    if (res.success) {
+      setActualUser(null);
     }
   }
-  
+
   return (
     <section className="container">
       <nav className="navbar">
@@ -23,38 +25,55 @@ const Navbar = () => {
           <a href="/" id="logo">
             You Rock
           </a>
-          <ul className="links">
-            <a href="/">Home</a>
-            <a href="/store">Products</a>
-            <a href="/about">About Us</a>
-          </ul>
+          <img src={menuIcon} alt="Abrir menu" id="menuIcon" onClick={()=> setOpenMenu(!openMenu)} />
+          <div className={openMenu ? 'links-authentication aparecer' : 'links-authentication'} >
+            <ul className="links">
+              <a href="/">Home</a>
+              <a href="/store">Products</a>
+              <a href="/about">About Us</a>
+            </ul>
+            {actualUser ? (
+              <div className="authentication">
+                <button className="cart-btn" onClick={() =>{
+                  if(openMenu){
+                    setOpenMenu(false)
+                  }
+                  openCart()
+                }}>
+                  <img src={cartIcon} alt="Carrito de compras" />
+                  <div className="cart-quantity">
+                    <p>{cartQuantity}</p>
+                  </div>
+                </button>
+                <p>
+                  <span>
+                    <button onClick={logout}>| Logout</button>
+                  </span>
+                </p>
+              </div>
+            ) : (
+              <div className="authentication">
+                <button className="cart-btn" onClick={() => {
+                  if(openMenu){
+                    setOpenMenu(false)
+                  }
+                  openCart()
+                }}>
+                  <img src={cartIcon} alt="Carrito de compras" />
+                  <div className="cart-quantity">
+                    <p>{cartQuantity}</p>
+                  </div>
+                </button>
+                <p>
+                  <a href="/signup">Sing up</a>{" "}
+                  <span>
+                    <a href="/login">| Login</a>
+                  </span>
+                </p>
+              </div>
+            )}
+          </div>
         </div>
-        {actualUser ? (
-          <div className="authentication">
-            <button className="cart-btn" onClick={()=> openCart()}>
-              <img src={cartIcon} alt="Carrito de compras" />
-              <div className="cart-quantity"><p>{cartQuantity}</p></div>
-            </button>
-          <p>
-            <span>
-              <button onClick={logout}>| Logout</button>
-            </span>
-          </p>
-        </div>
-        ): (
-          <div className="authentication">
-            <button className="cart-btn" onClick={()=> openCart()}>
-              <img src={cartIcon} alt="Carrito de compras" />
-              <div className="cart-quantity"><p>{cartQuantity}</p></div>
-            </button>
-          <p>
-            <a href="/signup">Sing up</a>{" "}
-            <span>
-              <a href="/login">| Login</a>
-            </span>
-          </p>
-        </div>
-        )}
       </nav>
     </section>
   );
