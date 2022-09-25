727f233a349cc24b4646319e3df11cae5367d4a5
Agustin notebook <ronedev25@gmail.com>
2022-09-25 11:48:12 -0300

    add: handle input change and function to copy to clipboard

--- client/src/components/ApiComponent/ApiInput.js
+++ client/src/components/ApiComponent/ApiInput.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import copyIcon from './../../images/icons/copying.png'
 
 const ApiInput = () => {
+  const [inputValue, setInputValue] = useState('')
+
+  const URL = 'https://you-rocks-backend.vercel.app/'
+
+  const copyUrlToClipboard = ()=> navigator.clipboard.writeText(URL + inputValue)
+
+  const handleInputChange = (e)=> setInputValue(e.target.value)
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+
+  }
+
   return (
     <section className='api-input'>
         <h3>¡Intentalo!</h3>
-        <form className='api-form'>
+        <form className='api-form' onClick={handleSubmit}>
             <label>https://you-rocks-backend.vercel.app/</label>
-            <input type="text" />
+            <input type="text" onChange={handleInputChange} />
             <div className="btn-flex">
-                <button className='btn'><img src={copyIcon} alt="Copiar URL" id='copy' /></button>
+                <button className='btn' onClick={copyUrlToClipboard}><img src={copyIcon} alt="Copiar URL" id='copy' /></button>
                 <button className='btn btn-background'>Enviar</button>
             </div>
         </form>
