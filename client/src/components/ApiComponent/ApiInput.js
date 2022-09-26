469173bfabad2d82fd77c47d01a9f2f6a9bf16c1
Agustin notebook <ronedev25@gmail.com>
2022-09-26 08:39:41 -0300

    add: handle bindings to make api calls

--- client/src/components/ApiComponent/ApiInput.js
+++ client/src/components/ApiComponent/ApiInput.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import JsonFormatter from "react-json-formatter";
 import { apiCall } from "../../services/product";
 import copyIcon from "./../../images/icons/copying.png";
 
 const ApiInput = () => {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("product/get");
   const [data, setData] = useState([]);
   const [alert, setAlert] = useState("");
 
@@ -26,13 +26,36 @@ const ApiInput = () => {
     }
   };
 
+  const handleLinks = async (e) => {
+    setInputValue(e.target.innerText)
+    const response = await apiCall(URL + e.target.innerText);
+    if (response.status === 200) {
+      setData(response.data);
+    } else {
+      setAlert(response.message);
+      setTimeout(() => setAlert(null), 5000);
+    }
+  };
+
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const response = await apiCall(URL + inputValue);
+        setData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    loadData();
+  }, []);
+
   const jsonStyle = {
-    propertyStyle: { color: "#C2FF0A", fontSize: '1.7rem' },
-    stringStyle: { color: "white", fontSize: '1.5rem' },
-    numberStyle: { color: "#dcff71", fontSize: '1.5rem' },
-    bracketStyle: {color: "#696969", fontSize: '2rem'},
-    braceStyle: { fontSize: '1.8rem'},
-    booleanStyle: { color: "white", fontSize: '1.5rem' }
+    propertyStyle: { color: "#C2FF0A", fontSize: "1.7rem" },
+    stringStyle: { color: "white", fontSize: "1.5rem" },
+    numberStyle: { color: "#dcff71", fontSize: "1.5rem" },
+    bracketStyle: { color: "#696969", fontSize: "2rem" },
+    braceStyle: { fontSize: "1.8rem" },
+    booleanStyle: { color: "white", fontSize: "1.5rem" },
   };
 
   return (
@@ -40,7 +63,7 @@ const ApiInput = () => {
       <h3>¡Intentalo!</h3>
       <form className="api-form" onClick={handleSubmit}>
         <label>https://you-rocks-backend.vercel.app/</label>
-        <input type="text" onChange={handleInputChange} />
+        <input type="text" onChange={handleInputChange} value={inputValue} />
         <div className="btn-flex">
           <button className="btn" onClick={copyUrlToClipboard}>
             <img src={copyIcon} alt="Copiar URL" id="copy" />
@@ -48,6 +71,14 @@ const ApiInput = () => {
           <button className="btn btn-background">Enviar</button>
         </div>
       </form>
+      <p>
+        Intenta con <span onClick={handleLinks}>product/get-offered</span> para
+        obtener los productos ofertados, o{" "}
+        <span onClick={handleLinks}>product/get-male</span> para los productos
+        masculinos, o{" "}
+        <span onClick={handleLinks}>product/search/?q=remera</span> para obtener
+        una busqueda en especifico
+      </p>
       {alert && <p>{alert}</p>}
       <section className="response">
         <div className="jsonContainer">
